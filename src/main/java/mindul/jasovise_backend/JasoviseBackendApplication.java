48da0d22228bf0c85ccce96f084159c198012008
Mindul <pencake33@naver.com>
2025-01-30 02:20:57 +0900

    first commit

---
+++ src/main/java/mindul/jasovise_backend/JasoviseBackendApplication.java
@@ -0,0 +1,13 @@
+package mindul.jasovise_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JasoviseBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JasoviseBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/mindul/jasovise_backend/JasoviseBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package mindul.jasovise_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JasoviseBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
