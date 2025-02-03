55a1e9e8f526d4ebd6eb0e17f810c4f96b1ddf7d
Mindul <pencake33@naver.com>
2025-02-04 01:59:25 +0900

    임시로 exception도 작업함. // 필요한 게 있는지는 확인해야 함

---
+++ src/main/java/mindul/jasovise_backend/exceptions/ResourceNotFoundException.java
@@ -0,0 +1,8 @@
+package mindul.jasovise_backend.exceptions;
+
+public class ResourceNotFoundException extends RuntimeException {
+  private static final long serialVersionUID = 1L;
+  public ResourceNotFoundException(String msg) {
+    super(msg);
+  }
+}
